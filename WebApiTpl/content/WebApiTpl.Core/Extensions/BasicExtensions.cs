3256f80fc513978fa58ed69b1eadbfda3e7a8e0c
zhoufeng <zhf883680@gmail.com>
2021-09-11 14:45:37 +0800

    add Fun GetbasicPath

---
+++ WebApiTpl/content/WebApiTpl.Core/Extensions/BasicExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WebApiTpl.Core.Extensions
+{
+    public class BasicExtensions
+    {
+        /// <summary>
+        /// 获取程序运行路径
+        /// </summary>
+        /// <returns></returns>
+        public static string GetBasicPath()
+        {
+            return $"{AppContext.BaseDirectory}/";
+        }
+    }
+}
